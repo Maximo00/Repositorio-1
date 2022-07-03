687fbe9cf9ba69cff4fde6483d5d5ce38cc3f63b
Maximo Ambrosio <101674799+mambrosio1@users.noreply.github.com>
2022-07-03 18:31:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
 function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
+    
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Remove the background color of all tablinks/buttons
+   
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "";
     }
   
-    // Show the specific tab content
+    
     document.getElementById(pageName).style.display = "block";
   
-    // Add the specific color to the button used to open the tab content
+    
     elmnt.style.backgroundColor = color;
   }
   
-  // Get the element with id="defaultOpen" and click on it
+  
   document.getElementById("defaultOpen").click();
+
+  google.charts.load('current',{packages:['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+  
+  function drawChart() {
+  
+  var data = google.visualization.arrayToDataTable([
+    ['Meses', 'Numero Ranking ATP'],
+    [1,13],[2,16],[3,15],[4,16],[5,15],
+    [6,15]
+  ]);
+  
+  var options = {
+    title: 'ATP Ranking 2022',
+    hAxis: {title: 'Meses'},
+    vAxis: {title: 'Numero Ranking ATP'},
+    legend: 'none'
+  };
+ 
+  var chart = new google.visualization.LineChart(document.getElementById('myChart'));
+  chart.draw(data, options);
+  }
+
+
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
